91be8c4199164c2ab8363c54948ffae300fd3024
Luisfetoga2 <l.torresg@uniandes.edu.co>
2024-09-28 10:12:54 -0500

    back finished

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,33 @@
-from matplotlib.colors import LinearSegmentedColormap
-from matplotlib import pyplot as plt
-from scipy.signal import savgol_filter
-from urllib.parse import unquote
-from pydantic import BaseModel
-from typing import Optional
-from fastapi import FastAPI
-import pandas as pd
-import numpy as np
 import matplotlib
-
-matplotlib.use('Agg')  
+from matplotlib.colors import LinearSegmentedColormap
+matplotlib.use('Agg')
+from typing import Optional
+from fastapi import FastAPI, UploadFile, File
+from urllib.parse import unquote
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.signal import savgol_filter
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+origins = [
+    'http://localhost/8000',
+    "http://127.0.0.1:8000",
+    'http://localhost:5173',
+    'http://localhost:5174'
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -22,24 +36,38 @@ def read_root():
 class CSVPath(BaseModel):
     path: str
 
-@app.get("/csv/")
-def process_csv(csv_path: CSVPath, q: Optional[str] = None):
+@app.post("/csv/")
+def process_csv(file: UploadFile = File(...), ruidoUmbral:int = -60, freqInicialOcupacion:int=435, freqFinalOcupacion:int=440, freqInicialPorcentaje:int=433, freqFinalPorcentaje:int=440):
+    # Leemos csv
+    data = file.file.read().decode("utf-8").replace("\r", "").split("\n")
+    
+    """
+    with open("output.csv", "w", encoding="utf-8") as f:
+        for line in data:
+            f.write(line + "\n") 
+    return
+    """
+    
+    index1 = data.index("")
+    header = data[:index1]
+    index2 = data[index1+1:].index("")+index1+1
+    fila = [x.split(";") for x in data[index1+2:index2]]
+    data3 = [x.split(";") for x in data[index2+1:]]
+    frequencies = [float(x[0].replace(",",".")) for x in fila]
+    matrix = []
+    for x in data3[3:-1]:
+        fila = []
+        for y in x[1:-1]:
+            y = y.replace(",",".")
+            fila.append(float(y))
+        matrix.append(fila)
+    
+    matrix = np.array(matrix, dtype="float64").T
+    matrix_sin_ruido = [x.copy() for x in matrix]
 
-    data = open(unquote(csv_path.path), "r").readlines()
-    index1 = data.index("\n")
-    index2 = data[index1 + 1: ].index("\n") + index1 + 1
+    ruido_promedio = quitarRuido(matrix_sin_ruido, ruidoUmbral)
 
-    filas = [x.split(";") for x in data[index1+2:index2]]
-
-    data = [x.split(";") for x in data[index2+1:]]
-    frequencies = [float(x[0].replace(",",".")) for x in filas]
-    matrix = np.array([[float(y.replace(",",".")) for y in x[1:-1]] for x in data[3:]]).T
-    matrix_sin_ruido = matrix.copy()
-
-    ruido_promedio = quitarRuido(matrix_sin_ruido,-60)
-
-    ##matrix = np.array([savgol_filter(x, window_length=51, polyorder=2) for x in matrix])
-
+    matrix_sin_ruido = np.array([savgol_filter(x, window_length=51, polyorder=2) for x in matrix_sin_ruido])
     
 
     return {
@@ -50,8 +78,8 @@ def process_csv(csv_path: CSVPath, q: Optional[str] = None):
         "snr": getSNR(matrix_sin_ruido, ruido_promedio),
         "forma_señal_ruido": getFormaSenal(matrix, 150, frequencies, isRuido=True),
         "forma_senal_no_ruido": getFormaSenal(matrix_sin_ruido, 150, frequencies),
-        "ocupacion": getOcupacion(matrix_sin_ruido, frequencies, 435*10**6, 440*10**6),
-        "porcentaje_banda": getPorcentajeBanda(matrix_sin_ruido, 432*10**6, 433*10**6, frequencies),
+        "ocupacion": getOcupacion(matrix_sin_ruido, frequencies, freqInicialOcupacion*10**6, freqFinalOcupacion*10**6),
+        "porcentaje_banda": getPorcentajeBanda(matrix_sin_ruido, freqInicialPorcentaje*10**6, freqFinalPorcentaje*10**6, frequencies),
         "espectograma_no_ruido": getEspectograma(matrix_sin_ruido, frequencies)
     }
 
@@ -79,7 +107,6 @@ def getFrecuenciaCentral(matrix, frequencies):
 def getFormaSenal(matrix, index, frequencies, isRuido = False):
 
     data = [round(x,2) for x in matrix[index,:].tolist()]
-    print(data)
     plt.plot(frequencies, data)
     plt.title("Forma de Señal")
     plt.xlabel("Frecuencia")
@@ -101,28 +128,31 @@ def getAnchoBanda(matrix, frequencies):
 
     for col in range(0,l//2):
         mayor_columna = np.max(matrix[:,col])
-        if mayor_columna != -100:
+        
+        if round(mayor_columna,2) != -100:
             index = 0
             while matrix[index][col] != mayor_columna:
                 index += 1
             break
     inicio = frequencies[col]
-    while matrix[index][col] != -100:
+
+    while round(matrix[index][col],2) != -100:
         col += 1
+
     fin = frequencies[col]
 
     izquierda = fin-inicio
 
     for col in range(l-1,l//2,-1):
         mayor_columna = np.max(matrix[:,col])
-        if mayor_columna != -100:
+        if round(mayor_columna,2) != -100:
             # Buscar fila
             index = 0
             while matrix[index][col] != mayor_columna:
                 index += 1
             break
     inicio = frequencies[col]
-    while matrix[index][col] != -100:
+    while round(matrix[index][col],2) != -100:
         col -= 1
     fin = frequencies[col]
 
